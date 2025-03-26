0f4b556dcf143ac625a6d2e4c006b267e0a11af2
polarbear0701 <phantnguyen2003@gmail.com>
2025-03-26 10:56:07 +0700

    chore: init project

---
+++ src/main/java/com/example/pawa_be/PawaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pawa_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pawa_be/PawaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pawa_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
