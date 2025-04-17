11fe816382a168b43e1abecede9681d3458b6567
Potemkin Pavel <potemkinpavel3@gmail.com>
2025-04-17 16:50:18 +0700

    change-project-folder-structure: add openapi wrappers

--- modules/profile/src/main/java/pawa_be/profile/internal/controller/PassengerController.java
+++ modules/profile/src/main/java/pawa_be/profile/internal/controller/PassengerController.java
@@ -1,14 +1,18 @@
 package pawa_be.profile.internal.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("")
+@Tag(name = "Profile Controller", description = "Operations about passengers")
 public class PassengerController {
 
     @GetMapping("")
+    @Operation(summary = "Greet passenger", description = "Returns \"Hello, Passenger!\" message.")
     public String greet(){
         return "Hello, Passenger!";
     }
