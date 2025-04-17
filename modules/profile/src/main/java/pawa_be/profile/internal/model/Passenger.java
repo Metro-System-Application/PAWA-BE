5d2d7da3ab0a6843d4e25be17027bf05532940dc
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2025-04-17 23:49:51 +0700

    Chore: Modify the Passenger model and Repository. Add Lombok to project pom.xml

--- modules/profile/src/main/java/pawa_be/profile/internal/model/Passenger.java
+++ modules/profile/src/main/java/pawa_be/profile/internal/model/Passenger.java
@@ -1,22 +1,55 @@
 package pawa_be.profile.internal.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+//import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+//import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="passenger")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Passenger {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String passengerID;
+
+    @Column(unique = true)
+    private String passengerEmail;
+
+    //User name
     private String passengerFirstName;
     private String passengerMiddleName;
     private String passengerLastName;
 
-    @Column(unique = true)
-    private String passengerEmail;
+    //User contact
     private String passengerPhone;
     private String passengerAddress;
+
+//  This json serialization is only for modifying the date format to DD/MM/YYYY if needed
+//    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate passengerDateOfBirth;
+
+    //User check for specific conditions
+    private Boolean hasDisability;
+    private Boolean isRevolutionary;
+
+    //User verification
+    private String nationalID;
+    private String studentID;
+    private String googleID;
+
+    //Account status
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
 
 }
