47182e545055150a7ad08ca049695798ecb661ab
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2025-04-17 20:52:15 +0700

    Feat: Add enumerator for ticket type

---
+++ modules/profile/src/main/java/pawa_be/profile/internal/enumerator/TicketType.java
@@ -0,0 +1,12 @@
+package pawa_be.profile.internal.enumerator;
+
+public enum TicketType {
+    ONE_WAY_4,
+    ONE_WAY_8,
+    ONE_WAY_X,
+    DAILY,
+    THREE_DAY,
+    MONTHLY_STUDENT,
+    MONTHLY_ADULT,
+    FREEEEEEEEEEEEE //I WILL HAVE IT, WE WILL HAVE IT!!!!!
+}
