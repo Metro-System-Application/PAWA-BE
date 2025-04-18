0d1a26023e484649d575f8d200977d7861ccff00
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2025-04-18 10:49:05 +0700

    Feat: Add Cart module

---
+++ modules/Cart/src/main/java/internal/model/CartModel.java
@@ -0,0 +1,30 @@
+package internal.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pawa_be.profile.internal.model.Passenger;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name="transaction")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CartModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID cartID;
+
+    @OneToOne
+    @JoinColumn(name="passenger_id", referencedColumnName = "passengerID", nullable = false)
+    private Passenger passenger;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ modules/Cart/src/main/java/internal/repository/CartRepository.java
@@ -0,0 +1,12 @@
+package internal.repository;
+
+import internal.model.CartModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CartRepository extends CrudRepository<CartModel, UUID> {
+
+}
