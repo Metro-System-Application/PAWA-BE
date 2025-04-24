23e98f446fcad7e97e5cb06d99ab74ed2738fe2a
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2025-04-24 13:52:37 +0700

    Feat: Add ticket-type api to fetch date for types of ticket (permit all temporarily)

--- modules/ticket/src/main/java/pawa_be/ticket/internal/dto/TypeDto.java
+++ modules/ticket/src/main/java/pawa_be/ticket/internal/dto/TypeDto.java
@@ -1,4 +1,32 @@
 package pawa_be.ticket.internal.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pawa_be.ticket.internal.Enumerator.TicketType;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TypeDto {
+    @Schema
+    private TicketType ticketType;
+
+    @Schema
+    private String typeName;
+
+    @Schema
+    private BigDecimal price;
+
+    @Schema
+    private String expiryDescription;
+
+    @Schema
+    private String requirementDescription;
+
 }
--- modules/ticket/src/main/java/pawa_be/ticket/internal/service/TicketService.java
+++ modules/ticket/src/main/java/pawa_be/ticket/internal/service/TicketService.java
@@ -1,4 +1,28 @@
 package pawa_be.ticket.internal.service;
 
+import org.springframework.stereotype.Service;
+import pawa_be.ticket.internal.Enumerator.TicketType;
+import pawa_be.ticket.internal.dto.TypeDto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class TicketService {
+    public List<TypeDto> getAllTicketType(){
+        return Arrays.stream(TicketType.values())
+                .map(this::convertTicketTypeDto)
+                .collect(Collectors.toList());
+    }
+
+    private TypeDto convertTicketTypeDto(TicketType ticketType) {
+        return new TypeDto(
+                ticketType,
+                ticketType.getDisplayedName(),
+                ticketType.getPrice(),
+                ticketType.getExpiryDescription(),
+                ticketType.getEligibilityRequirements()
+        );
+    }
 }
