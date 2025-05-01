510529afdd1e506d1d0373d8912af8da1df54fe7
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2025-05-02 01:56:32 +0700

    Chore: Clean up code

--- modules/ticket/src/main/java/pawa_be/ticket/external/enumerator/TicketType.java
+++ modules/ticket/src/main/java/pawa_be/ticket/external/enumerator/TicketType.java
@@ -1,8 +1,5 @@
 package pawa_be.ticket.external.enumerator;
 
-/**
- * Ticket type codes only - all properties are now stored in the database
- */
 public enum TicketType {
         ONE_WAY_4,
         ONE_WAY_8,
