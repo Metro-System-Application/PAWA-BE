8f61fcd21f716c2df06fca44d76df52f447ee9f3
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2025-05-18 14:14:04 +0700

    chore: add item sort

---
+++ modules/payment/src/main/java/pawa_be/payment/internal/enumeration/InvoiceItemSortField.java
@@ -0,0 +1,20 @@
+package pawa_be.payment.internal.enumeration;
+
+public enum InvoiceItemSortField {
+    ID("invoiceItemID"),
+    TICKET_TYPE("ticketType"),
+    PURCHASED_AT("purchasedAt"),
+    ACTIVATED_AT("activatedAt"),
+    EXPIRED_AT("expiredAt"),
+    PRICE("price");
+
+    private final String fieldName;
+
+    InvoiceItemSortField(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+} 
